6d3fbb916c1cd120300b571f67d23b0570ad05e7
Joe Sanfilippo <joe.sanfilippo@gmail.com>
2012-10-11 14:43:11 -0700

    Enabled myBatis logging by adding log4j based logging to the project. Converted all system.out calls to logging calls.

--- src/main/java/com/jsanfil/photoshare/rest/PhotoResource.java
+++ src/main/java/com/jsanfil/photoshare/rest/PhotoResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,12 +27,16 @@ public class PhotoResource {
 	@Autowired
 	PhotoMapper photoMapper;
 	
+	Logger logger = Logger.getLogger(this.getClass());
+	
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON})
 	public List<Photo> findAll() {
-		System.out.println("findAll");
+		logger.debug("findAll");
 		return photoMapper.selectAll();
 	}
+	
+
 
 /***
 	@GET @Path("search/{query}")
@@ -46,33 +51,34 @@ public class PhotoResource {
 	@Produces({ MediaType.APPLICATION_JSON})
 	// TODO change return value back to Photo
 	public Photo findById(@PathParam("id") String id) {
-		System.out.println("findById " + id);
+		logger.debug("findById " + id);
 		Photo output = photoMapper.selectByPrimaryKey(Integer.valueOf(id));
 		return output;
 	}
 
 	@POST
-	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Consumes({ MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON})
 	public Photo create(Photo photo) {
-		System.out.println("creating Photo");
+		logger.debug("creating Photo");
 		int id = photoMapper.insert(photo);
 		photo.setId(id);
 		return photo;
 	}
 
 	@PUT @Path("{id}")
-	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Consumes({ MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON})
 	public Photo update(Photo Photo) {
-		System.out.println("Updating Photo: " + Photo.getTitle());
+		logger.debug("Updating Photo: " + Photo.getTitle());
 		photoMapper.updateByPrimaryKey(Photo);
 		return Photo;
 	}
 	
 	@DELETE @Path("{id}")
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON})
 	public void remove(@PathParam("id") String id) {
+		logger.debug("Removing Photo: " +id);
 		photoMapper.deleteByPrimaryKey(Integer.valueOf(id));
 	}
 
